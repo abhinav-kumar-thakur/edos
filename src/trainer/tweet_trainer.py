e7bf4be3edb48450a8ba12985f07617ad0b67344
abhinav-kumar-thakur <abhinavkumarthakur786@gmail.com>
2022-12-18 16:31:48 -0800

    Update predictions to save

--- src/trainer/tweet_trainer.py
+++ src/trainer/tweet_trainer.py
@@ -27,7 +27,7 @@ class TweetTrainer(Trainer):
             pred, _ = self.model(batch, train=False)
             actual_labels.extend(batch['label_sexist'])
             predicted_labels.extend(pred)
-            predictions.extend(zip(batch['text'], pred))
+            predictions.extend(zip(batch['text'] if self.configs.model.type == 'bert' else batch['question'], pred))
         
         scores = classification_report(actual_labels, predicted_labels, output_dict=True)
 
