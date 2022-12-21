7c7b5e3f55b9aaff6da635d119d33ea6bc8cc655
abhinav-kumar-thakur <abhinavkumarthakur786@gmail.com>
2022-12-20 16:15:14 -0800

    Log model performance in console

--- src/models/utils.py
+++ src/models/utils.py
@@ -7,6 +7,7 @@ from src.models.unifiedQA import UnifiedQAClassifier
 
 
 def get_classification_model(configs, state_configs, device):
+    print('Loading model...')
     model_name = configs.model.type
 
     if model_name == 'bert':
@@ -17,6 +18,7 @@ def get_classification_model(configs, state_configs, device):
         raise Exception('Invalid model name')
 
     if state_configs.epoch > 0:
+        print(f'Loading saved model for {state_configs.kth_fold} fold and {state_configs.epoch} epoch')
         saved_model_path = os.path.join(configs.logs.dir, configs.title + '-' + configs.task, configs.logs.files.models, f'saved_model_state.pt')
         model.load_state_dict(torch.load(saved_model_path))
 
--- src/trainer/trainer.py
+++ src/trainer/trainer.py
@@ -59,6 +59,7 @@ class Trainer(ABC):
                 
                 self.logger.log_file(self.configs.logs.files.train, {"Kth Fold": kth_fold, "Epoch": epoch, 'train': train_scores, 'loss': avg_loss})
                 self.logger.log_file(self.configs.logs.files.train, {"Kth Fold": kth_fold, "Epoch": epoch, 'eval': eval_scores})
+                self.logger.log(f'Train Score: {train_scores} \n\n Eval Score: {eval_scores} \n\n Best Score: {self.state_configs.best_score} \n\n Epochs Without Improvement: {self.state_configs.epochs_without_improvement}')
                 save_model(self.model, self.configs)
                 self.state_configs.edit('epoch', epoch + 1)
                 write_json_configs(self.state_configs, os.path.join(self.logger.dir, self.configs.logs.files.state))
